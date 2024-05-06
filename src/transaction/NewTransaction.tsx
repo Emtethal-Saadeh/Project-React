4136057dd4c2958eca73a05906c4ec7445eb15b8
AbdullahDawoud <abdullahdawoud@gmail.com>
2024-05-06 17:15:55 +0300

    refactor new transaction

--- src/transaction/NewTransaction.tsx
+++ src/transaction/NewTransaction.tsx
@@ -54,62 +54,65 @@ const NewTransaction = () => {
 
   return (
     <>
-      <div className="vh-100 d-flex justify-content-center align-items-center">
-        <div className="row bg-white p-4 rounded-5 ">
-          <h1 className="m-0">
-            <img className="img3" src={sideimag} alt="Avatar" /> Transactions Table
-          </h1>
-          <div className="bg-white rounded-5 ">
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={onSubmit}>
-              {({ isSubmitting, isValid, dirty, setFieldValue, values }) => (
-                <Form>
-                  <div className="form-group m-1">
-                    <label>Name</label>
-                    <Field type="text" name="name" className="form-control " />
-                    <ErrorMessage name="name" component="div" className="text-danger" />
+      <div className="p-2 pt-3">
+        <header className="mb-3 d-flex justify-content-between align-items-center ">
+          <div className="fs-1 fw-bold">Transactions</div>
+
+          <Link to="/transaction/new">
+            <div className="btn btn-primary">New Transaction</div>
+          </Link>
+        </header>
+
+        <div className="panel w-100">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            {({ isSubmitting, isValid, dirty, setFieldValue, values }) => (
+              <Form>
+                <div className="form-group m-1">
+                  <label>Name</label>
+                  <Field type="text" name="name" className="form-control w-200px" />
+                  <ErrorMessage name="name" component="div" className="text-danger" />
+                </div>
+                <div className="form-group m-1">
+                  <label>Category</label>
+                  <Field as="select" name="category" className="form-control">
+                    <option value="">Select category</option>
+                    <option value="Supermarket">Supermarket</option>
+                    <option value="Personal">Personal</option>
+                    <option value="Home and Entertainment">Home and Entertainment</option>
+                  </Field>
+                  <ErrorMessage name="category" component="div" className="text-danger" />
+                </div>
+                <div className="form-group m-1">
+                  <label>Date</label>
+                  <div className="">
+                    <Calendar
+                      value={values.date}
+                      onChange={async (e) => await setFieldValue('date', e.value)}
+                    />
                   </div>
-                  <div className="form-group m-1">
-                    <label>Category</label>
-                    <Field as="select" name="category" className="form-control">
-                      <option value="">Select category</option>
-                      <option value="Supermarket">Supermarket</option>
-                      <option value="Personal">Personal</option>
-                      <option value="Home and Entertainment">Home and Entertainment</option>
-                    </Field>
-                    <ErrorMessage name="category" component="div" className="text-danger" />
-                  </div>
-                  <div className="form-group m-1">
-                    <label>Date</label>
-                    <div className="">
-                      <Calendar
-                        value={values.date}
-                        onChange={async (e) => await setFieldValue('date', e.value)}
-                      />
-                    </div>
-                    <ErrorMessage name="date" component="div" className="text-danger" />
-                  </div>
-                  <div className="form-group m-1">
-                    <label>Amount</label>
-                    <Field type="number" name="amount" step="1" className="form-control" />
-                    <ErrorMessage name="amount" component="div" className="text-danger" />
-                  </div>
-                  <button
-                    type="submit"
-                    className="btn color m-2 btn1"
-                    disabled={isSubmitting || !(isValid && dirty)}>
-                    Submit
-                  </button>
-                  <Link to="/transactions" className="btn btn-secondary btn1">
-                    Cancel
-                  </Link>
-                </Form>
-              )}
-            </Formik>
-            {successMessage.length > 0 && <div className="text-success">{successMessage}</div>}
-          </div>
+                  <ErrorMessage name="date" component="div" className="text-danger" />
+                </div>
+                <div className="form-group m-1">
+                  <label>Amount</label>
+                  <Field type="number" name="amount" step="1" className="form-control" />
+                  <ErrorMessage name="amount" component="div" className="text-danger" />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary color m-2 "
+                  disabled={isSubmitting || !(isValid && dirty)}>
+                  Submit
+                </button>
+                <Link to="/transactions" className="btn btn-light ">
+                  Cancel
+                </Link>
+              </Form>
+            )}
+          </Formik>
+          {successMessage.length > 0 && <div className="text-success">{successMessage}</div>}
         </div>
       </div>
     </>
